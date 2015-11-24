ba461307c58d0193874d3fec9082d3ffd9c5cf89
Greg Marshall <gmarsh@truman.edu>
2015-11-24 12:22:42 -0600

    Removed Slide transition

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-	<div id="slide-carousel" class="carousel slide" data-ride="carousel" data-pause="false" data-interval="8000">
+	<div id="slide-carousel" class="carousel" data-ride="carousel" data-pause="false" data-interval="8000">
 
 		<!-- Wrapper for slides -->
 		<div class="carousel-inner" role="listbox">
